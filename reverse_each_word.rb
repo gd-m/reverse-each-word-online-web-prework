56ac1fb0c31299addedde1631ebeee58dbb919a9
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-02-21 01:56:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,26 +1,16 @@
+def reverse_each_word(sentence)
+  new_sen = sentence.split(" ")
+  new_sen.collect do |word|
+    word.reverse
+  end
+  new_sen.join(" ")
+end
+def reverse_each_word(sentence)
+  new_sen = sentence.split(" ")
+  reverse_array = []
+  new_sen.each do |word|
+    reverse_array.push(word.reverse)
+  end
+  reverse_array.join(" ")
 
- def reverse_each_word(sentence)
-     new_sen1 = sentence.split(" ")
-     reverse_array = []
-     new_sen1.each do |word|
-       reverse_array.push(word.reverse)
-    end
-     reverse_array.join(" ")
-
-  new_sen2 = sentence.split(" ")
-  new_sen2.collect do |word|
-    new_sen2 << word.reverse
-   end
-  new_sen2.join(" ")
-
- end
-
-# def reverse_each_word(sentence)
-#  new_sen = sentence.split(" ")
-#  reverse_array = []
-#  new_sen.each do |word|
-#    reverse_array.push(word.reverse)
-#  end
-#  reverse_array.join(" ")
-
-# end
+end
